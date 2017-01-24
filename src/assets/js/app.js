373daf7023bc20c772e5a098c40939e1c3e4c4bc
Jan Babinec <jan.babinec@usetreno.cz>
2017-01-24 14:00:19 +0100

    template: homepage

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,8 +2,6 @@
 window.jQuery = window.$ = require('jquery');
 //external
 require('babel-polyfill');
-require('slick-carousel');
-
 //local
 require('./scripts/layout');
 require('./scripts/banner');
