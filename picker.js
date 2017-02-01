ee4bddde608d559ba3cd7bed2ea480c43897740d
janbabinec <babinecjan@icloud.com>
2017-02-01 21:56:52 +0100

    _template: add some styling

--- picker.js
+++ picker.js
@@ -1,4 +1,4 @@
-console.log(findFactors("#666666", "#333333", "$color-black", 10));
+console.log(findFactors("#666666", "#fdfeff", "$color-black", 10));
 
 function RGBtoHSL(r, g, b) {
     r = r / 255;
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,11 +2,14 @@
 window.jQuery = window.$ = require('jquery');
 //external
 require('babel-polyfill');
+require('select2');
 
 //local
 require('./scripts/banner');
 require('./scripts/layout');
 require('./scripts/scroll');
-
-
 require('./scripts/eshop-slider');
+
+$('select').select2({
+});
+$.fn.select2.defaults.set("theme", "classic");
