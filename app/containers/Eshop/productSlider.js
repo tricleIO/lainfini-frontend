0ee246831bdbfb4c9e1d3ae04263b8bbef10ae5a
Michal Fikejs <michal@fikejs.com>
2017-03-23 22:54:42 +0100

    Slider update

--- app/containers/Eshop/productSlider.js
+++ app/containers/Eshop/productSlider.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import _ from 'lodash';
 
-import 'slick-carousel';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
 
 import config from 'config';
 
@@ -12,13 +12,47 @@ export default class ProductSlider extends React.PureComponent { // eslint-disab
     imgs: React.PropTypes.array,
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      actualImg: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.timer = window.setInterval(() => this.findNextImg(), 5000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timer);
+  }
+
+  findNextImg() {
+    const count = _(this.props.imgs).size() - 1;
+
+    if (this.state.actualImg < count) {
+      this.setState({
+        actualImg: this.state.actualImg + 1,
+      });
+    } else {
+      this.setState({
+        actualImg: 0,
+      });
+    }
+  }
+
   render() {
     return (
       <div className="product-list__slider" ref={(c) => { this.productListSlider = c; }}>
-        { _(this.props.imgs).size() > 0 &&
-          <img src={config.apiUrl + 'files/' + _(this.props.imgs).sample().fileIndex + '.jpg'} alt="" />
+        {_(this.props.imgs).size() > 0 &&
+          <ReactCSSTransitionReplace
+            transitionName="cross-fade" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}
+          >
+            <img src={config.apiUrl + 'files/' + this.props.imgs[this.state.actualImg].fileIndex + '.jpg'} alt="" />
+          </ReactCSSTransitionReplace>
         }
-        { _(this.props.imgs).size() === 0 &&
+        {_(this.props.imgs).size() === 0 &&
           <img src="http://placehold.it/356x387" alt="" />
         }
       </div>
