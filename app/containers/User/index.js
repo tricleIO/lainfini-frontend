9da55fc6bcccea126a5fcab1ba2f94030e4a202c
Michal Fikejs <michal@fikejs.com>
2017-03-26 17:09:41 +0200

    Deleted comment

--- app/containers/User/index.js
+++ app/containers/User/index.js
@@ -71,24 +71,6 @@ class User extends React.Component {
 
 }
 
-/* <div>
-        <div className="text-center">
-          <Heading type="h1" title={<FormattedMessage {...messages.title} />} />
-        </div>
-
-        <div className="user-panel">
-          <div className="login-panel">
-            <LoginForm onSubmit={this.handleSubmit} />
-          </div>
-          <div className="register-panel">
-
-          </div>
-        </div>
-
-        <SocialNav links />
-      </div>
-      */
-
 function mapDispatchToProps(dispatch) {
   return {
     loginUser: (email, password) => dispatch(loginUser(email, password)),
