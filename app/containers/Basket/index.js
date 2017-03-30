c4713865dacd83671000f788a7450e60b9f1d8fe
Michal Fikejs <michal@fikejs.com>
2017-03-30 17:43:12 +0200

    Added missing page titles

--- app/containers/Basket/index.js
+++ app/containers/Basket/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import SocialNav from 'components/SocialNav';
 import LastView from 'components/LastView';
@@ -41,6 +42,7 @@ class Basket extends React.Component {
     }
     return (
       <main id="page">
+        <Helmet title="Basket" />
         <div className="basket">
           <div className="product-list">
             <div className="container">
