426dbda4ef9f9152af35bef54fc498f542eb46e5
Michal Fikejs <michal@fikejs.com>
2017-04-02 11:52:25 +0200

    Register form validation

--- app/containers/User/forms/register.js
+++ app/containers/User/forms/register.js
@@ -9,18 +9,24 @@ class RegisterForm extends React.Component {
 
   render() {
     const { handleSubmit } = this.props;
+
+    const required = (value) => value ? undefined : 'Required';
+    const email = (value) =>
+      value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+        'Invalid email address' : undefined;
+
     return (
       <form className="proceed_box" onSubmit={handleSubmit}>
         <h3 className="mb-3">New Customer</h3>
         <p className="mb-5">Sign up to place your order and receive exclusive offers.</p>
         <div className="form-group">
-          <Field component="input" name="email" type="email" className="form-control" placeholder="Your@email" />
+          <Field component="input" name="email" type="email" className="form-control" validate={[required, email]} placeholder="Your@email" />
         </div>
         <div className="form-group">
-          <Field component="input" name="full-name" type="text" className="form-control" placeholder="Full name" />
+          <Field component="input" name="full-name" type="text" className="form-control" validate={[required]} placeholder="Full name" />
         </div>
         <div className="form-group">
-          <Field component="input" name="password" type="password" className="form-control" placeholder="Password" />
+          <Field component="input" name="password" type="password" className="form-control" validate={[required]} placeholder="Password" />
         </div>
         <button className="btn btn-block mt-4 mb-4 text-uppercase">Sign Up</button>
         <p className="text-center">Or sign in with your existing social media account</p>
